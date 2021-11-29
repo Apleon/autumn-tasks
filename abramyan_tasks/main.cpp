1072cdbeb72228fbebc8eb195c2bca23566303f5
Apleon <shara49750@gmail.com>
2021-11-29 23:52:03 +0300

    Initial Commit

---
+++ abramyan_tasks/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  abramyan_tasks
+//
+//  Created by Андрей Шарапов on 29.11.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
