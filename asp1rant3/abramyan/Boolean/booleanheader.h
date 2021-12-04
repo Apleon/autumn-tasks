00fe019b7ffd00e28325dc0ddae2e73390cf1048
asp1rant3 <asp1rantzenevich@gmail.com>
2021-12-04 11:24:09 +0300

    added boolean fucntions body

--- asp1rant3/abramyan/Boolean/booleanheader.h
+++ asp1rant3/abramyan/Boolean/booleanheader.h
@@ -6,75 +6,120 @@ using namespace std;
 void mission_1(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_2(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_3(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_4(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_5(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_6(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_7(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_8(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_9(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_10(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_11(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_12(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_13(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_14(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_15(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_16(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_17(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_18(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_19(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_20(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_21(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_22(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_23(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_24(){
     setlocale(LC_ALL, "");
 }
-void mission_1(){
+void mission_25(){
+    setlocale(LC_ALL, "");
+}
+void mission_26(){
+    setlocale(LC_ALL, "");
+}
+void mission_27(){
+    setlocale(LC_ALL, "");
+}
+void mission_28(){
+    setlocale(LC_ALL, "");
+}
+void mission_29(){
+    setlocale(LC_ALL, "");
+}
+void mission_30(){
+    setlocale(LC_ALL, "");
+}
+void mission_31(){
+    setlocale(LC_ALL, "");
+}
+void mission_32(){
+    setlocale(LC_ALL, "");
+}
+void mission_33(){
+    setlocale(LC_ALL, "");
+}
+void mission_34(){
+    setlocale(LC_ALL, "");
+}
+void mission_35(){
+    setlocale(LC_ALL, "");
+}
+void mission_36(){
+    setlocale(LC_ALL, "");
+}
+void mission_37(){
+    setlocale(LC_ALL, "");
+}
+void mission_38(){
+    setlocale(LC_ALL, "");
+}
+void mission_39(){
+    setlocale(LC_ALL, "");
+}
+void mission_40(){
     setlocale(LC_ALL, "");
 }
